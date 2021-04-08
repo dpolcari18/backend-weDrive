c72f13910a5f5c288b5d5a72d8edac2e1d9a5e62
David Polcari <dpolcari18@gmail.com>
2021-04-07 21:09:25 -0500

    register user is working with error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
 
         # check to see if user_params pass validations
-        if user.valid?
-            user.save
+        if user.save
             render json: { status: 'Success', msg: 'Thank you for signing up. Please sign in!' }
         else
             render json: { status: 'Failed', msg: user.errors.full_messages }
