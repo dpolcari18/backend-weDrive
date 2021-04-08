b84ee3301a0312a9ef5695154ffa63d5d81e4f50
David Polcari <dpolcari18@gmail.com>
2021-04-07 20:19:52 -0500

    create user route working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
 class UsersController < ApplicationController
+    # skip authentication when signing up
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        user = User.new(user_params)
+
+        # check to see if user_params pass validations
+        if user.valid?
+            user.save
+            render json: { status: 'Success', msg: 'Thank you for signing up. Please sign in!' }
+        else
+            render json: { status: 'Failed', msg: user.errors.full_messages }
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :phone, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :locations
-  resources :segments
-  resources :trips
-  resources :maintenance_reports
-  resources :vehicles
-  resources :emergency_contacts
-  resources :users
-  resources :sessions
+  # resources :locations
+  # resources :segments
+  # resources :trips
+  # resources :maintenance_reports
+  # resources :vehicles
+  # resources :emergency_contacts
+  resources :users, only: [:create]
+  resources :sessions, only: [:create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
