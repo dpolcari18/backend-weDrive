3a38928c047747d97f891ec6251b375318af407c
David Polcari <dpolcari18@gmail.com>
2021-04-14 00:07:22 -0500

    added logo hyperlink to both emails

--- app/mailers/trip_detail_mailer.rb
+++ app/mailers/trip_detail_mailer.rb
@@ -5,6 +5,11 @@ class TripDetailMailer < ApplicationMailer
         @trip = params[:trip]
         @locations = params[:locations]
         @segments = params[:segments]
+        
+        attachments.inline['logo'] = {
+            data: File.read(Rails.root.join('app/assets/images/horizontal_logo.png')),
+            mime_type: 'image/png'
+        }
 
         mail(to: @emergency_contact.email, subject: @user.first_name + ' started a trip on weDrive!')
     end
@@ -13,7 +18,12 @@ class TripDetailMailer < ApplicationMailer
         @emergency_contact = params[:ec]
         @user = params[:user]
         @trip = params[:trip]
-        @locations = params[:locations]   
+        @locations = params[:locations]
+
+        attachments.inline['logo'] = {
+            data: File.read(Rails.root.join('app/assets/images/horizontal_logo.png')),
+            mime_type: 'image/png'
+        }   
 
         mail(to: @emergency_contact.email, subject: @user.first_name + ' finished their trip on weDrive!')
     end
