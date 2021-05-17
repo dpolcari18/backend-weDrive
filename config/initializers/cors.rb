e3b4c0ff34f229b9e8a477c7b555be57bb80633d
David Polcari <dpolcari18@gmail.com>
2021-05-17 10:40:06 -0500

    changed cors origin to netlify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    origins 'https://wedrive.netlify.app'
 
     resource '*',
       headers: :any,
