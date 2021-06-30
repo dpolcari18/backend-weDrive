baf865e5033213b7ee3802931be406e9f5bfdb25
David Polcari <dpolcari18@gmail.com>
2021-06-29 19:18:46 -0500

    added local host to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins 'https://wedrive.netlify.app'
+    origins 'https://wedrive.netlify.app', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
